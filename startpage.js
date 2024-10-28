425eb37ee5ae55747b2481873d093ff4b64d8eff
manupriyarc <manupriyarchitragar@gmail.com>
2024-10-28 21:05:32 +0530

    Final2

--- startpage.js
+++
@@ -1,4 +0,0 @@
-// JavaScript for additional functionalities (future usage)
-document.querySelector(".order-btn").addEventListener("click", function() {
-    alert("SignUp first and then place your order!");
-});
